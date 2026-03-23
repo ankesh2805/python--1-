5310e2e2e770a223ca6bef865fcec41b091a8756
Ankesh <ankesh2805@gmail.com>
2026-03-23 17:24:01 +0530

    ankesh

---
+++ class_1/one.py
@@ -0,0 +1,20 @@
+ename=["Rahul","Sonia","Priyanka"] #list
+uname=() #tupple
+eid={101,102,103,104} #set
+emp={"eid":101,"ename":"rahul","esla":45000} #dict
+for key ,value  in emp.items():
+  print(key , value)
+for key  in emp.keys():
+  print(key)
+for value  in emp.values():
+  print(value)
+
+
+
+
+# ename[0]="Rahul Gandhi"
+# print(ename)
+
+# eid.add(105)
+# print(eid)
+# print(emp[lo])
---
+++ class_1/two.py
@@ -0,0 +1,4 @@
+eid ={101,102,103,104}
+numbers={10,"rahul",40.545,False }
+print(eid)
+print(numbers)
